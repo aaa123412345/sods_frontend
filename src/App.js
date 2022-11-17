15033db76383666863c99dd8c407ccbd091c1462
aaa123412345 <yim.201314@gmail.com>
2022-11-18 02:34:05 +0800

    Fix the redirect

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React from "react";
 import PublicPageManager from "./components/PublicSite/PublicPageManager/PublicPageManager";
 import ServerPageManager from "./components/ServerSite/ServerPageManager/ServerPageManager";
 
-import { BrowserRouter, Link, Routes, Route, redirect} from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
 
 
 function App() {
 
-    
+    /*
+    <Route path='/public' element={<PublicPageManager />}></Route> 
+                <Route path='/' element={<PublicPageManager />}></Route> 
+    */
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/public' element={<PublicPageManager />}></Route> 
-                <Route path='/' element={<PublicPageManager />}></Route> 
+                
                 <Route path='/public/:path' element={<PublicPageManager />}></Route> 
                 <Route path='/server/*' element={<ServerPageManager/>}></Route>
+                <Route
+                    path="*"
+                    element={
+                        <Navigate replace to="/public/about" />
+                    }
+                    />
             </Routes>
             
         </BrowserRouter>
