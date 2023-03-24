cb02ca97e49622bd59402b9f1e91b2cea2187ab9
aaa123412345 <yim.201314@gmail.com>
2023-03-24 21:29:27 +0800

    Add video config panel

--- src/components/CMS/PageEditor/PageDetailEditor/PEConfigModal/PEConfigComponent/PEConfigVideo.js
+++ src/components/CMS/PageEditor/PageDetailEditor/PEConfigModal/PEConfigComponent/PEConfigVideo.js
@@ -1,4 +1,4 @@
-import { Tab,Row,Col,Tabs } from "react-bootstrap"
+import { Tab,Row,Col,Tabs,Tooltip,OverlayTrigger } from "react-bootstrap"
 import PEConfigTabBasic from "./PEConfigTabBasic"
 import PEConfigTabPreview from "./PEConfigTabPreview"
 
@@ -10,12 +10,54 @@ const PEConfigVideo = ({editData,command}) => {
                     {PEConfigTabBasic({editData:editData,command:command})}
                     <Tab eventKey="detail" title="Detail">
                         <Row>
-                            <Col></Col>
+                            <Col>
+
+                                <OverlayTrigger
+                                key={"SRC-tip"}
+                               
+                                overlay={
+                                    <Tooltip id={`tooltip-srctip`} >
+                                        {"(Format: https://www.youtube.com/embed/{video code} or local file : /assets/video/{video name}"}
+                                    </Tooltip>
+                                }
+                                >
+                                <h1 style={{fontSize:"1.5vw"}}>{"src: "}</h1>
+                                </OverlayTrigger>
+                               
+                                <input type="text" value={editData.src} style={{border:'solid black 1px'}}
+                                    onChange={(e)=>command.setEditDataInChild("src",e.target.value)}>
+                                </input>
+                                <h1 style={{fontSize:"1.5vw"}}>{"alt: "}</h1>
+                                <input type="text" value={editData.alt} style={{border:'solid black 1px'}}
+                                    onChange={(e)=>command.setEditDataInChild("alt",e.target.value)}>
+                                </input>
+                                <h1 style={{fontSize:"1.5vw"}}>{"Video Config: "}</h1>
+                                <input type="checkbox" checked={editData.autoplay}
+                                onChange={(e)=>command.setEditDataInChild("autoplay",e.target.checked)} 
+                                style={{paddingLeft:"10px",paddingRight:"10px"}}></input> {"Autoplay "}
+
+                                <input type="checkbox" checked={editData.controls}
+                                onChange={(e)=>command.setEditDataInChild("controls",e.target.checked)}
+                                style={{paddingLeft:"10px",paddingRight:"10px"}}></input> {"Controls "}
+                                <input type="checkbox" checked={editData.loop}
+                                onChange={(e)=>command.setEditDataInChild("loop",e.target.checked)}
+                                style={{paddingLeft:"10px",paddingRight:"10px"}}></input> {"Loop "}
+                                <input type="checkbox" checked={editData.muted}
+                                onChange={(e)=>command.setEditDataInChild("muted",e.target.checked)}
+                                style={{paddingLeft:"10px",paddingRight:"10px"}}></input> {"Muted "}
+
+                               
+                            </Col>
                         </Row>
                     </Tab>
                     <Tab eventKey="style" title="Style">
                         <Row>
-                            <Col></Col>
+                            <Col>
+                            <h1 style={{fontSize:"1.5vw"}}>{"Width: "}</h1>
+                                <input type="text" value={editData.style.width} style={{border:'solid black 1px'}}
+                                    onChange={(e)=>command.setStyleData("width",e.target.value)}>
+                                </input>
+                            </Col>
                         </Row>
                     </Tab>
                     {PEConfigTabPreview({editData:editData,command:command})}
--- src/components/Common/LayoutElement/CVideo/CVideo.js
+++ src/components/Common/LayoutElement/CVideo/CVideo.js
@@ -7,18 +7,38 @@ const CVideo = (props) => (
 export default CVideo
 
 function generator(props){
+    function checkSRC(src){
+        //check if src contains http:// or https://
+        if(src.includes("http://") || src.includes("https://")){
+            return true
+        }else{
+            return false
+        }
+    }
     return(
         <div className="cvideo">  
-            <video style={props.block.style} 
+            {checkSRC(props.block.src) ?
+            <iframe 
+            style={props.block.style}
+            src={props.block.src}
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+            </iframe>:
+             <video style={props.block.style} 
             src={props.block.src} 
             alt={props.block.alt}
             controls={props.block.controls}
             autoPlay={props.block.autoPlay}
             muted={props.block.muted}
-            >
-           
+            > 
             Your browser does not support the video tag.
             </video>
+            }
+           
+           
+           
             
         </div>
     )
